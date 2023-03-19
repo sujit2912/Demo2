7b2b8df603ca2489f4757a6da089e49fe43c75e8
sujit2912 <123256034+sujit2912@users.noreply.github.com>
2023-03-19 22:52:38 +0530

    Add files via upload

---
+++ variables/R_variables.R
@@ -0,0 +1,61 @@
+######Variables#####
+#Variables are containers for storing the data values.
+#Variable are used to allocate a memory location to a specific
+#value that we want to store.
+#A variable must start with a letter.
+#It can contain underscore,letters and numbers.
+#It cannot contain - and all other special characters,
+#like #,@,!,$,%,^,&,*,etc.
+#In R we can use both (= and <-) as a assignment operator, but
+#mostly <- operator is preffered over =.
+
+
+#Data types in R:
+#1.Integer
+#2.Float
+#3.String/character
+#4.Logical/Boolean
+
+
+#Data structures in R
+#Vector and Data Frame, Lists
+
+
+
+
+
+#Accessing elements from a vector
+
+
+#Assigning or replacing values in a vector
+
+ 
+
+#List Creation
+
+
+#Accessing and replacing elements from list
+
+
+
+#Data Frame Creation
+
+
+#Accessing elements from a data frame
+
+
+
+#Replacing values from a data frame.
+
+
+
+
+#In-class activity
+#1. Perform basic arithmetic operations
+#2. Create variables and store various values and their data type.
+#3. Create atomic vector,lists,data frame
+#4. Perform indexing/slicing on list
+#5. Create a data frame with 3 columns and 5 records.
+#6. Perform 5 slicing operations on data frames.
+#7. Replace some values from data frame
+
